c62e12c92c01d970125b3729fb03c5d7542fb218
Daria Musaeva <hipperhipper@mail.ru>
2023-05-14 17:47:12 +0500

    csv added

--- src/test/java/ru/netology/services/ThresholdServiceTest.java
+++ src/test/java/ru/netology/services/ThresholdServiceTest.java
@@ -1,26 +1,17 @@
 package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class ThresholdServiceTest {
 
-    @Test
-    public void testShouldWorkFirst() {
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/threshold.csv")
+    public void testShouldWork (int expected, int income, int expenses, int threshold) {
         ThresholdService service = new ThresholdService();
 
-        int expected = 3;
-        int actual = service.calcThreshold(10_000, 3_000, 20_000);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testShouldWorkSecond() {
-        ThresholdService service = new ThresholdService();
-
-        int expected = 2;
-        int actual = service.calcThreshold(100_000, 60_000, 150_000);
+        int actual = service.calcThreshold(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
